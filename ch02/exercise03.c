6907fd70714b337a04ab326c97709117f7762104
Makihara <limuyuan@users.noreply.github.com>
2016-11-16 14:18:45 +0800

    Create exercise03.c

---
+++ ch02/exercise03.c
@@ -0,0 +1,11 @@
+//Programming Exercese 2.12.3
+#include <stdio.h>
+int main(void)
+{
+	int age, days;
+	
+	age = 25;
+	days = 365 * age;
+	printf("You've past about %d days since you born!\n", age);
+	return 0;
+}
