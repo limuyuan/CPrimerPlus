f2c9895b69ec9210a1e34426765310f263f6d7d3
Makihara <limuyuan@users.noreply.github.com>
2016-11-16 14:33:29 +0800

    Create exercise06.c

---
+++ ch02/exercise06.c
@@ -0,0 +1,10 @@
+//Programming Exercise 2.12.6
+#include <stdio.h>
+int main(void)
+{
+	int toes;
+	
+	toes = 10;
+	printf("toes is %d, toes squared is %d, toes cubed is %d", toes, toes * toes, toes * toes * toes);
+	return 0;
+}
