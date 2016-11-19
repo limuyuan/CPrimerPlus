de3c069075023d5cdc7b5fef3acd19cf2ee35d70
limuyuan <limy1912@mails.jlu.edu.cn>
2016-11-19 15:01:08 +0800

    deleate codes

--- ch03/exercise03.c
+++
@@ -1,8 +0,0 @@
-//Programming Exercise 3.11.3
-#include <stdio.h>
-int main(void)
-{
-	printf("\aStartled by the sudden sound, Sally shouted,\n\"By the Great Pumpkin, what was that!\"\n");
-
-	return 0;
-}
